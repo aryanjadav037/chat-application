07847f9c09bd5d1c9eebaef5a8ac5689d1c23e8a
aryan <aryanjadav081103@gmail.com>
2025-04-15 17:12:16 +0530

    handle group msg

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,9 +1,9 @@
-
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  groupId: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: false },
+  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
   message: { type: String, required: true },
 }, { timestamps: true });
 
--- backend/service/messageService.js
+++ backend/service/messageService.js
@@ -1,19 +1,46 @@
 export default class MessageService {
-    constructor(MessageModel) {
-      this.MessageModel = MessageModel;
-    }
-  
-    async createMessage(senderId, receiverId, message) {
-      return this.MessageModel.create({ senderId, receiverId, message });
-    }
-  
-    async getChat(senderId, receiverId) {
-      return this.MessageModel.find({
-        $or: [
-          { senderId, receiverId },
-          { senderId: receiverId, receiverId: senderId },
-        ],
-      }).sort({ createdAt: 1 });
-    }
+  constructor(MessageModel) {
+    this.MessageModel = MessageModel;
   }
-  
+
+  async createMessage(senderId, receiverId, message) {
+    return this.MessageModel.create({ senderId, receiverId, message });
+  }
+
+  async createGroupMessage(senderId, groupId, message) {
+    return this.MessageModel.create({ senderId, groupId, message });
+  }
+
+  async getChat(senderId, receiverId) {
+    return this.MessageModel.find({
+      $or: [
+        { senderId, receiverId },
+        { senderId: receiverId, receiverId: senderId },
+      ],
+    })
+    .sort({ createdAt: 1 })
+    .populate('senderId', 'username profilePic')
+    .populate('receiverId', 'username profilePic');
+  }
+
+  async getGroupChat(groupId) {
+    return this.MessageModel.find({ groupId })
+      .sort({ createdAt: 1 })
+      .populate('senderId', 'username profilePic');
+  }
+
+  async markMessageAsRead(messageId) {
+    return this.MessageModel.findByIdAndUpdate(
+      messageId,
+      { read: true },
+      { new: true }
+    );
+  }
+
+  async getUnreadMessageCount(userId) {
+    return this.MessageModel.countDocuments({
+      receiverId: userId,
+      read: false
+    });
+  }
+}
